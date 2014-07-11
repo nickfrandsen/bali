d98a3851b2c524cc987910ae26a288a5f4904492
Nicholas Frandsen <nick.rowe.frandsen@gmail.com>
2014-07-11 15:12:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bali::Application.config.secret_key_base = '76ce2a387a252ca416dce543cbbf31f45743ef71a37995ade9b21201a0fd4a55134189aad1e415218f60adf66d15deb2253b331e8240efa3228fe02bee9165cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bali::Application.config.session_store :cookie_store, key: '_bali_session'
