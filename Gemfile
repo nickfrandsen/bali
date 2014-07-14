801556adcbfe06b6d8cf39af1f1d954354a2435f
Nicholas Frandsen <nick.rowe.frandsen@gmail.com>
2014-07-14 19:20:57 +0200

    Added devise.

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ end
 
 gem "font-awesome-rails"
 gem 'rails_12factor', group: :production
+gem 'devise'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Bali::Application.routes.draw do
+  devise_for :users
   resources :blog_entries, path: 'blog'
 
   # The priority is based upon order of creation: first created -> highest priority.
